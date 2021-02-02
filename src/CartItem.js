1477dc435ed711dcccda874b46f0af79be9db557
Tau <hectauduque@gmail.com>
2021-02-02 18:12:09 -0300

    final changes

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,36 +1,46 @@
 import React from "react";
+import CartItem from "./CartItem";
 import { useGlobalContext } from "./context";
-const CartItem = ({ id, img, title, price, amount }) => {
-  const { remove, increase, decrease } = useGlobalContext();
+
+const CartContainer = () => {
+  const { cart, total, clearCart } = useGlobalContext();
+  if (cart.length === 0) {
+    return (
+      <section className="cart">
+        {/* cart header */}
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
   return (
-    <article className="cart-item">
-      <img src={img} alt={title} />
+    <section className="cart">
+      {/* cart header */}
+      <header>
+        <h2>your bag</h2>
+      </header>
+      {/* cart items */}
       <div>
-        <h4>{title}</h4>
-        <h4 className="item-price">${price}</h4>
-        {/* remove button */}
-        <button className="remove-btn" onClick={() => remove(id)}>
-          remove
-        </button>
+        {cart.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
       </div>
-      <div>
-        {/* increase amount */}
-        <button className="amount-btn" onClick={() => increase(id)}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z" />
-          </svg>
+      {/* cart footer */}
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>${total}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn" onClick={clearCart}>
+          clear cart
         </button>
-        {/* amount */}
-        <p className="amount">{amount}</p>
-        {/* decrease amount */}
-        <button className="amount-btn" onClick={() => decrease(id)}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-          </svg>
-        </button>
-      </div>
-    </article>
+      </footer>
+    </section>
   );
 };
 
-export default CartItem;
+export default CartContainer;
